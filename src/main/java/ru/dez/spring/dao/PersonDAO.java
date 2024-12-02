ea0de55d9fa07143f47dd12456d4c6bd6ba48b83
DeZ <zhilindanilw@gmail.com>
2024-12-03 06:55:04 +0800

    Реализовал добавление пользователей

--- src/main/java/ru/dez/spring/dao/PersonDAO.java
+++ src/main/java/ru/dez/spring/dao/PersonDAO.java
@@ -61,8 +61,13 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "insert into person VALUES ("+1+",'" + person.getName() + "',"+person.getAge()+",'"+person.getEmail()+"')";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void update(int id, Person updatedPerson) {
