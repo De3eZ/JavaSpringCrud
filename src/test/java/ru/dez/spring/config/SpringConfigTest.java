b3d32776a3616ae96ec0e0ac4f41fa248efb2530
DeZ <zhilindanilw@gmail.com>
2025-02-10 22:26:54 +0800

    Добавление тестов на SpringConfig

---
+++ src/test/java/ru/dez/spring/config/SpringConfigTest.java
@@ -0,0 +1,38 @@
+package ru.dez.spring.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringConfigTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void templateResolver() {
+    }
+
+    @Test
+    void templateEngine() {
+    }
+
+    @Test
+    void configureViewResolvers() {
+    }
+
+    @Test
+    void dataSource() {
+    }
+
+    @Test
+    void jdbcTemplate() {
+    }
+}
