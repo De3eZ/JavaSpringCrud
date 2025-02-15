fa05796d43c9d5872c040ee4a6b495929aa832e1
DeZ <zhilindanilw@gmail.com>
2025-02-15 21:48:26 +0800

    Добавление тестов для Person

---
+++ src/test/java/ru/dez/spring/models/PersonTest.java
@@ -0,0 +1,40 @@
+package ru.dez.spring.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
